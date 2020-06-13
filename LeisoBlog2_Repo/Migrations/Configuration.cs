f7bcd5e3299557e42c9c67a779d51d1de457449d
Lesio Programista <lama7410@gmail.com>
2020-06-13 13:49:11 +0200

    #LB test

--- LeisoBlog2_Repo/Migrations/Configuration.cs
+++ LeisoBlog2_Repo/Migrations/Configuration.cs
@@ -18,7 +18,6 @@ namespace LesioBlog2_Repo.Migrations
         protected override void Seed(BlogContext context)
         {
             //  This method will be called after migrating to the latest version.
-
             //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
             //  to avoid creating duplicate seed data.
             SeedGenders(context);
